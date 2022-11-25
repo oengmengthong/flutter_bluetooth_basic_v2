16df67c0ac64f8e223ce1da4fbdff4022191d0ab
Oeng Mengthong <40915642+oengmengthong@users.noreply.github.com>
2022-11-25 10:16:41 +0700

    publish first version to pub.dev

--- test/flutter_bluetooth_basic_test.dart
+++ test/flutter_bluetooth_basic_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_bluetooth_basic_v2_v2/flutter_bluetooth_basic_v2.dart';
+import 'package:flutter_bluetooth_basic_v2/flutter_bluetooth_basic_v2.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_bluetooth_basic_v2');
