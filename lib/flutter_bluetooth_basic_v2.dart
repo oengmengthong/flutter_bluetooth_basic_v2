7a29929a73ddb0d7e3905a0afd7c2632305ee894
Oeng Mengthong <40915642+oengmengthong@users.noreply.github.com>
2022-11-25 10:44:39 +0700

    update

--- lib/flutter_bluetooth_basic_v2.dart
+++ lib/flutter_bluetooth_basic_v2.dart
@@ -1,14 +1,11 @@
-// You have generated a new plugin project without specifying the `--platforms`
-// flag. A plugin project with no platform support was generated. To add a
-// platform, run `flutter create -t plugin --platforms <platforms> .` under the
-// same directory. You can also find a detailed instruction on how to add
-// platforms in the `pubspec.yaml` at
-// https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+/*
+ * flutter_bluetooth_basic_v2
+ * Created by Oeng Mengthong.
+ * 
+ * See LICENSE for distribution and usage details.
+ */
 
-import 'flutter_bluetooth_basic_v2_platform_interface.dart';
+library flutter_bluetooth_basic_v2;
 
-class FlutterBluetoothBasicV2 {
-  Future<String?> getPlatformVersion() {
-    return FlutterBluetoothBasicV2Platform.instance.getPlatformVersion();
-  }
-}
+export './src/bluetooth_manager.dart';
+export './src/bluetooth_device.dart';
--- test/flutter_bluetooth_basic_v2_test.dart
+++ test/flutter_bluetooth_basic_v2_test.dart
@@ -1,29 +1,19 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bluetooth_basic_v2/flutter_bluetooth_basic_v2.dart';
-import 'package:flutter_bluetooth_basic_v2/flutter_bluetooth_basic_v2_platform_interface.dart';
-import 'package:flutter_bluetooth_basic_v2/flutter_bluetooth_basic_v2_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-class MockFlutterBluetoothBasicV2Platform
-    with MockPlatformInterfaceMixin
-    implements FlutterBluetoothBasicV2Platform {
-
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
 
 void main() {
-  final FlutterBluetoothBasicV2Platform initialPlatform = FlutterBluetoothBasicV2Platform.instance;
+  const MethodChannel channel = MethodChannel('flutter_bluetooth_basic_v2');
 
-  test('$MethodChannelFlutterBluetoothBasicV2 is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelFlutterBluetoothBasicV2>());
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
   });
 
-  test('getPlatformVersion', () async {
-    FlutterBluetoothBasicV2 flutterBluetoothBasicV2Plugin = FlutterBluetoothBasicV2();
-    MockFlutterBluetoothBasicV2Platform fakePlatform = MockFlutterBluetoothBasicV2Platform();
-    FlutterBluetoothBasicV2Platform.instance = fakePlatform;
-
-    expect(await flutterBluetoothBasicV2Plugin.getPlatformVersion(), '42');
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
   });
 }
